8c37bf7923cec1c3147496c57bd2a81d4627e6f8
yakut özcan <yakutozcan@gmail.com>
2017-08-15 20:51:57 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'IoT IYTE',
+    title: 'IoT',
     condition: true,
     anyArray: [1, 2, 3]
   });
